bf387d1b2173b3a3619a83e9ce71a9d12d75fcdd
trung.hoang <hoangtrung.huster@gmail.com>
2020-02-02 01:26:30 +0700

    Updates

---
+++ precache-manifest.06e22450f2ae2d4c06f7a81454d2b81c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7058464fe50a8b1ea6e26dedab81f320",
+    "url": "/forecast-weather/index.html"
+  },
+  {
+    "revision": "1b5194200a4ba0bf8b78",
+    "url": "/forecast-weather/static/css/main.d64fb327.chunk.css"
+  },
+  {
+    "revision": "8a58663d8493a307f238",
+    "url": "/forecast-weather/static/js/2.5fca071e.chunk.js"
+  },
+  {
+    "revision": "1b5194200a4ba0bf8b78",
+    "url": "/forecast-weather/static/js/main.93e7653a.chunk.js"
+  },
+  {
+    "revision": "73799fdf2d6d6fd36d50",
+    "url": "/forecast-weather/static/js/runtime~main.130fb685.js"
+  },
+  {
+    "revision": "51de28c28db40d5e2d61d75fb9be7d14",
+    "url": "/forecast-weather/static/media/wall.51de28c2.jpg"
+  }
+]);
